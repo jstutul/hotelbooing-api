cd4fc670702a0dc869822992519a3c92e9a991d5
jstutul <jaforsadiktutul9228@gmail.com>
2022-07-05 12:03:46 +0600

    admin verify done

--- routes/users.js
+++ routes/users.js
@@ -5,11 +5,17 @@ import {
   getUsers,
   updateUser,
 } from "../controller/user.js";
-import { verifyToken } from "../utils/verifyToken.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 const router = express.Router();
 
 router.get("/checkauthentication", verifyToken, (req, res, next) => {
   res.send("hello user, you are login");
+});
+router.get("/checkuser/:id", verifyUser, (req, res, next) => {
+  res.send("hello user, you are login and you can delete your account");
+});
+router.get("/checkadmin/:id", verifyAdmin, (req, res, next) => {
+  res.send("hello admin, you are login and you can delete all account");
 });
 //GET ALL
 router.get("/", getUsers);
--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -3,7 +3,6 @@ import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
-  console.log(token);
   if (!token) {
     return next(createError(401, "you are not authenticcated"));
   }
@@ -13,3 +12,24 @@ export const verifyToken = (req, res, next) => {
     next();
   });
 };
+export const verifyUser=(req,res,next)=>{
+  verifyToken(req,res,()=>{
+    if(req.user.id === req.params.id && req.user.isAdmin){
+      next()
+    }
+    else{
+      return next(createError(403, "You are not allow to delete user account"));
+    }
+  })
+}
+
+export const verifyAdmin=(req,res,next)=>{
+  verifyToken(req,res,()=>{
+    if(req.user.isAdmin){
+      next()
+    }
+    else{
+      return next(createError(403, "You are not allow to delete user account"));
+    }
+  })
+}
