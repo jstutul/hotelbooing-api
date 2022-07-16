923eaee7c7f1c7e80d90bc48efbf2b104daf421e
jstutul <jaforsadiktutul9228@gmail.com>
2022-07-16 11:20:27 +0600

    Api Finish initial

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,7 +1,18 @@
 import express from "express";
+import { createRoom, deleteRoom, getRoom, getRooms, updateRoom } from "../controller/room.js";
+import { createError } from "../utils/error.js";
+import { verifyAdmin} from "../utils/verifyToken.js";
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("This is Rooms Router Home Page");
-});
+//CREATE
+router.post("/:hotelid",verifyAdmin,  createRoom);
+//UPDATE
+router.put("/:id",verifyAdmin,  updateRoom);
+//DELETE
+router.delete("/:id/:hotelid",verifyAdmin, deleteRoom);
+//GET
+router.get("/:id", getRoom);
+//GET ALL
+
+router.get("/", getRooms);
 export default router;
