df9fd04f5cb392a8241dc2d4f94746ce352a7228
jstutul <jaforsadiktutul9228@gmail.com>
2022-07-17 17:59:17 +0600

    Reach Fetch using URL Location

--- controller/hotel.js
+++ controller/hotel.js
@@ -42,8 +42,10 @@ export const getHotel=async (req,res,next)=>{
 }
 
 export const getHotels=async (req,res,next)=>{
+  const {min,max,...others}=req.query;
+  console.log(min,max);
   try {
-    const hotels = await Hotel.find();
+    const hotels = await Hotel.find({...others,cheapestPrice:{$gt : min|1, $lt:max||1000},}).limit(req.query.limit);
     res.status(200).json(hotels);
   } catch (err) {
     next(err)
@@ -62,8 +64,18 @@ export const countByCity=async (req,res,next)=>{
 }
 export const countByType=async (req,res,next)=>{
   try {
-    const hotels = await Hotel.find();
-    res.status(200).json(hotels);
+    const hotelCount=await Hotel.countDocuments({type:"hotel"});
+    const apartmentCount=await Hotel.countDocuments({type:"apartment"});
+    const resortCount=await Hotel.countDocuments({type:"resort"});
+    const villaCount=await Hotel.countDocuments({type:"villa"});
+    const cabinCount=await Hotel.countDocuments({type:"cabin"});
+    res.status(200).json([
+      {type:"hotel",count:hotelCount},
+      {type:"apartment",count:apartmentCount},
+      {type:"resort",count:resortCount},
+      {type:"villa",count:villaCount},
+      {type:"cabin",count:cabinCount},
+    ]);
   } catch (err) {
     next(err)
   }
